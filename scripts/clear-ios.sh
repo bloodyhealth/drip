b8543402d35d3da1b66a97413f84eed3be45e70d
Maria Zadnepryanets <mariya.z@gmail.com>
2022-08-15 18:12:35 +0200

    Update ios clear wording and remove unnecessary command

--- scripts/clear-ios.sh
+++ scripts/clear-ios.sh
@@ -14,8 +14,8 @@ rm -rf ios/Pods/*
 echo "Remove Podfile.lock..."
 rm Podfile.lock
 
-echo "Wipe iOS build artifacts..."
-rm -rf ios/build && (killall Xcode || true) && xcrun -k && cd ios && xcodebuild -alltargets clean && cd .. && rm -rf "$(getconf DARWIN_USER_CACHE_DIR)/org.llvm.clang/ModuleCache" && rm -rf "$(getconf DARWIN_USER_CACHE_DIR)/org.llvm.clang.$(whoami)/ModuleCache" && rm -fr ~/Library/Developer/Xcode/DerivedData/ && rm -fr ~/Library/Caches/com.apple.dt.Xcode/
+echo "Close XCode and wipe iOS build artifacts..."
+(killall Xcode || true) && xcrun -k && cd ios && xcodebuild -alltargets clean && cd .. && rm -rf "$(getconf DARWIN_USER_CACHE_DIR)/org.llvm.clang/ModuleCache" && rm -rf "$(getconf DARWIN_USER_CACHE_DIR)/org.llvm.clang.$(whoami)/ModuleCache" && rm -fr ~/Library/Developer/Xcode/DerivedData/ && rm -fr ~/Library/Caches/com.apple.dt.Xcode/
 
 echo "Wipe system iOS Pods cache..."
 pod cache clean --all
