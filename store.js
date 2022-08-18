8de4bda42fc0cf55c516ed9e4172db9bbd3da6f1
Sofiya Tepikin <sofiya.tepikin@gmail.com>
2022-08-18 10:33:12 +0200

    Cleanup date from the store

--- store.js
+++ store.js
@@ -1,10 +1,8 @@
 import { combineReducers, createStore } from 'redux'
 
-import date from './slices/date'
 import navigation from './slices/navigation'
 
 const reducer = combineReducers({
-  date,
   navigation,
 })
 
