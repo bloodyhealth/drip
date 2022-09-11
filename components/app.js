3524489860944d1656ab8e722a2b144c7cd67a07
MariaZ <mariya.z@gmail.com>
2022-09-11 12:40:10 +0200

    Make App a functional component

--- components/app.js
+++ components/app.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
 import { BackHandler, StyleSheet, View } from 'react-native'
 import PropTypes from 'prop-types'
 
@@ -13,40 +13,22 @@ import { headerTitles } from '../i18n/en/labels'
 import setupNotifications from '../lib/notifications'
 import { closeDb } from '../db'
 
-class App extends Component {
-  static propTypes = {
-    date: PropTypes.string,
-    navigate: PropTypes.func,
-    setDate: PropTypes.func,
-    goBack: PropTypes.func,
-    restartApp: PropTypes.func,
-  }
+const App = ({ restartApp }) => {
+  const [date, setDate] = useState(LocalDate.now().toString())
+  const [currentPage, setCurrentPage] = useState('Home')
 
-  constructor(props) {
-    super(props)
+  setupNotifications(setCurrentPage, setDate)
 
-    this.backHandler = BackHandler.addEventListener(
+  useEffect(() => {
+    const backHandler = BackHandler.addEventListener(
       'hardwareBackPress',
-      this.goBack
+      goBack
     )
 
-    this.state = {
-      date: LocalDate.now().toString(),
-      currentPage: 'Home',
-    }
+    return () => backHandler.remove()
+  }, [])
 
-    setupNotifications(this.navigate, this.setDate)
-  }
-
-  navigate = (page) => {
-    this.setState({ currentPage: page })
-  }
-
-  setDate = (date) => {
-    this.setState({ date })
-  }
-
-  goBack = () => {
+  const goBack = () => {
     const { currentPage } = this.state
 
     if (currentPage === 'Home') {
@@ -54,52 +36,43 @@ class App extends Component {
       BackHandler.exitApp()
     } else {
       const { parent } = pages.find((p) => p.component === currentPage)
-      this.navigate(parent)
+      setCurrentPage(parent)
     }
 
     return true
   }
 
-  componentWillUnmount() {
-    this.backHandler.remove()
+  if (!currentPage) return false
+
+  const Page = viewsList[currentPage]
+
+  const isSettingsSubView = isSettingsView(currentPage)
+  const isTemperatureEditView = currentPage === 'TemperatureEditView'
+
+  const headerProps = {
+    title: headerTitles[currentPage],
+    handleBack: isSettingsSubView ? goBack : null,
+    navigate: setCurrentPage,
   }
 
-  render() {
-    const { goBack, restartApp } = this.props
-    const { date, currentPage } = this.state
-    const { navigate } = this
-
-    if (!currentPage) {
-      return false
-    }
-
-    const Page = viewsList[currentPage]
-    const title = headerTitles[currentPage]
-
-    const isSettingsSubView = isSettingsView(currentPage)
-    const isTemperatureEditView = currentPage === 'TemperatureEditView'
-
-    const headerProps = {
-      title,
-      handleBack: isSettingsSubView ? goBack : null,
-      navigate,
-    }
-
-    const pageProps = {
-      date,
-      setDate: this.setDate,
-      isTemperatureEditView,
-      navigate,
-    }
-
-    return (
-      <View style={styles.container}>
-        <Header {...headerProps} />
-        <Page {...pageProps} restartApp={restartApp} />
-        <Menu currentPage={currentPage} navigate={navigate} />
-      </View>
-    )
+  const pageProps = {
+    date,
+    setDate,
+    isTemperatureEditView,
+    navigate: setCurrentPage,
   }
+
+  return (
+    <View style={styles.container}>
+      <Header {...headerProps} />
+      <Page {...pageProps} restartApp={restartApp} />
+      <Menu currentPage={currentPage} navigate={setCurrentPage} />
+    </View>
+  )
+}
+
+App.propTypes = {
+  restartApp: PropTypes.func,
 }
 
 const styles = StyleSheet.create({
