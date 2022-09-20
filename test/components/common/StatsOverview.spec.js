e2e320927b4e3b33534f11b8187c8218b3fc7e9a
MariaZ <mariya.z@gmail.com>
2022-09-20 10:59:28 +0200

    Add tests for StatsOverview component

---
+++ test/components/common/StatsOverview.spec.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { render } from '@testing-library/react-native'
+import StatsOverview from '../../../components/common/StatsOverview'
+
+jest.mock('react-i18next', () => ({
+  useTranslation: () => ({
+    t: (str, options) => {
+      return str + (options ? JSON.stringify(options) : '')
+    },
+  }),
+}))
+
+describe('StatsOverview screen', () => {
+  test('when provided correct, renders it', async () => {
+    const data = [
+      [21, 'shortest'],
+      [21, 'longest'],
+      [0, 'standard deviation'],
+      [2, 'completed cycles'],
+    ]
+    const { toJSON } = render(<StatsOverview data={data} />)
+
+    expect(toJSON()).toMatchSnapshot()
+  })
+
+  test('when provided empty data, renders nothing (does not break)', async () => {
+    const data = []
+    const { toJSON } = render(<StatsOverview data={data} />)
+
+    expect(toJSON()).toMatchSnapshot()
+  })
+})
