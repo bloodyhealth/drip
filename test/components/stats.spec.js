5f61f37d2f2a6a3a785cd91e0673917c751d5509
MariaZ <mariya.z@gmail.com>
2022-09-20 12:31:54 +0200

    Add tests for Stats component

---
+++ test/components/stats.spec.js
@@ -0,0 +1,55 @@
+import React from 'react'
+import { render } from '@testing-library/react-native'
+
+import Stats from '../../components/stats'
+
+jest.mock('../../components/common/AppHelp', () => 'AppHelp')
+jest.mock('../../components/common/StatsOverview', () => 'StatsOverview')
+jest.mock('../../components/common/StatsTable', () => 'StatsTable')
+jest.mock('../../assets/cycle-icon.png', () => 'image')
+
+jest.mock('react-i18next', () => ({
+  useTranslation: () => ({
+    t: (str, options) => str + (options ? JSON.stringify(options) : ''),
+  }),
+}))
+
+const mockGetAllCycleLengths = jest
+  .fn()
+  .mockImplementationOnce(() => [])
+  .mockImplementationOnce(() => [30, 31, 30])
+  .mockImplementationOnce(() => null)
+  .mockImplementationOnce(() => undefined)
+
+jest.mock('../../lib/cycle', () => ({
+  __esModule: true,
+  default: () => ({
+    getAllCycleLengths: mockGetAllCycleLengths,
+  }),
+}))
+
+describe('Stats screen', () => {
+  test('when provided no data, renders no_data text', async () => {
+    const { toJSON } = render(<Stats />)
+
+    expect(toJSON()).toMatchSnapshot()
+  })
+
+  test('when provided data, renders stats', async () => {
+    const { toJSON } = render(<Stats />)
+
+    expect(toJSON()).toMatchSnapshot()
+  })
+
+  test('when provided null, renders no_data text', async () => {
+    const { toJSON } = render(<Stats />)
+
+    expect(toJSON()).toMatchSnapshot()
+  })
+
+  test('when provided undefined, renders no_data text', async () => {
+    const { toJSON } = render(<Stats />)
+
+    expect(toJSON()).toMatchSnapshot()
+  })
+})
