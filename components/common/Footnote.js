0185c7ec859fddad2742862c4a3dffefc3fd9af9
MariaZ <mariya.z@gmail.com>
2022-09-20 20:57:18 +0200

    Add light option for Footnote

--- components/common/Footnote.js
+++ components/common/Footnote.js
@@ -7,13 +7,15 @@ import Asterisk from '../common/Asterisk'
 
 import { Colors, Spacing } from '../../styles'
 
-const Footnote = ({ children }) => {
+const Footnote = ({ light, children }) => {
   if (!children) return false
+
+  const textStyle = light ? styles.lightText : styles.text
 
   return (
     <View style={styles.container}>
       <Asterisk />
-      <AppText linkStyle={styles.link} style={styles.text}>
+      <AppText linkStyle={styles.link} style={textStyle}>
         {children}
       </AppText>
     </View>
@@ -22,6 +24,7 @@ const Footnote = ({ children }) => {
 
 Footnote.propTypes = {
   children: PropTypes.node,
+  light: PropTypes.bool,
 }
 
 const styles = StyleSheet.create({
@@ -29,14 +32,17 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     alignContent: 'flex-start',
     marginBottom: Spacing.tiny,
-    marginTop: Spacing.small,
+    marginTop: Spacing.base,
+  },
+  lightText: {
+    color: Colors.greyLight,
+    paddingLeft: Spacing.small,
   },
   link: {
     color: 'white',
   },
   text: {
-    color: Colors.greyLight,
-    paddingLeft: Spacing.base,
+    paddingLeft: Spacing.small,
   },
 })
 
