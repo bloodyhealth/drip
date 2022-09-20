13a9cbd89b03d4be3bc49dbce3c77d1da61721dd
MariaZ <mariya.z@gmail.com>
2022-09-20 18:36:25 +0200

    Update test name and remove unnesessary async

--- test/components/common/Footnote.spec.js
+++ test/components/common/Footnote.spec.js
@@ -3,15 +3,15 @@ import { render } from '@testing-library/react-native'
 
 import Footnote from '../../../components/common/Footnote'
 
-describe('Footnote screen', () => {
-  test('when children are present, renders them', async () => {
+describe('Footnote component', () => {
+  test('when children are present, renders them', () => {
     const text = 'Some footnote text'
     const { toJSON } = render(<Footnote>{text}</Footnote>)
 
     expect(toJSON()).toMatchSnapshot()
   })
 
-  test('when no children, renders nothing', async () => {
+  test('when no children, renders nothing', () => {
     const { toJSON } = render(<Footnote></Footnote>)
 
     expect(toJSON()).toMatchSnapshot()
