010cabcefbe1644654e145d239856cdb2dae5431
bl00dymarie <ml.kochsiek@mailbox.org>
2024-02-15 17:37:15 +0100

    Allow scrolling in note text field

--- components/cycle-day/symptom-edit-view.js
+++ components/cycle-day/symptom-edit-view.js
@@ -110,7 +110,7 @@ const SymptomEditView = ({ date, onClose, symptom, symptomData }) => {
   const inputProps = {
     multiline: true,
     numberOfLines: 3,
-    scrollEnabled: false,
+    scrollEnabled: true,
     style: styles.input,
     textAlignVertical: 'top',
   }
