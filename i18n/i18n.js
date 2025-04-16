fb960da51b85c2f21bace320531a8308dc021b37
Lisa <lisa.hillebrand@posteo.de>
2025-04-16 11:02:27 +0000

    760 update i18n dependencies and config

--- i18n/i18n.js
+++ i18n/i18n.js
@@ -5,19 +5,16 @@ import { initReactI18next } from 'react-i18next'
 import en from './en.json'
 
 const resources = {
-  en: { translation: en },
+  en_US: { translation: en },
 }
 
 i18n
-  // pass the i18n instance to react-i18next.
   .use(initReactI18next)
   // init i18next
   // for all options read: https://www.i18next.com/overview/configuration-options
   .init({
-    compatibilityJSON: 'v3', // TODO: migrate json to v4 and afterwards remove it
     resources,
-    fallbackLng: 'en',
-
+    fallbackLng: 'en_US',
     interpolation: {
       escapeValue: false, // not needed for react as it escapes by default
     },
