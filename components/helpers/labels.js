0c13019b47de2181bae4a3570abba642a5178760
Lisa Hillebrand <lisa.hillebrand@posteo.de>
2025-04-23 14:48:06 +0200

    774 translate bleeding

--- components/helpers/labels.js
+++ components/helpers/labels.js
@@ -1,2 +1,18 @@
+import i18n from '../../i18n/i18n'
+
+export const SYMPTOMS = {
+  bleeding: {
+    heaviness: ['spotting', 'light', 'medium', 'heavy'],
+  },
+}
+
 export const getLabelsList = (categories) =>
   categories.map((label, i) => ({ label, value: i }))
+
+export const getLabelsListNew = (category, subCategory) => {
+  const symptoms = SYMPTOMS[category][subCategory]
+  return symptoms.map((symptom, i) => ({
+    label: i18n.t(`cycleDay.${category}.${subCategory}.symptoms.${symptom}`),
+    value: i,
+  }))
+}
