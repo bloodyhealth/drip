ec7b60ee89c51e6899622566c8c661ddad053029
Lisa Hillebrand <lisa.hillebrand@posteo.de>
2026-03-03 22:56:24 +0100

    fix ios build script

--- scripts/reinstall-project.sh
+++ scripts/reinstall-project.sh
@@ -5,8 +5,8 @@ echo "\x1b[35;01m""Start re-installing dependencies...""\x1b[39;49;00m"
 echo "Remove node_modules..."
 rm -rf node_modules
 
-echo "Yarn install..."
-yarn
+echo "PNPM install..."
+pnpm install
 
 echo "Pods install..."
 cd ios && pod install && cd ..
